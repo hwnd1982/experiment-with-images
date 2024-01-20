f1033b52488850bf41430d24b5d48a3bfa27c759
Kirill Lavrov <lkv@myttk.ru>
2024-01-21 00:12:33 +0700

    experiment completed

--- main.js
+++ main.js
@@ -5,19 +5,20 @@ const imgs = wrapp.querySelectorAll('img');
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext("2d", { willReadFrequently: true });
 
-imgs.forEach(img => {
+const setSlideBackground = img => {
+  const slide = img.closest('.slide');
+  
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.drawImage(img, 0, 0, img.width, img.height)
 
-  img.onload = () => {
-    const slide = img.closest('.slide');
-  
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0, img.width, img.height)
-  
-    const [r, g, b, a] = ctx.getImageData(0, 0, 1, 1).data;
-    console.log(r, g, b, a);
-  
-    slide.style.cssText = `background-color: rgba(${r}, ${g}, ${b}, ${a/255});`;
-  }
+  const [r, g, b, a] = ctx.getImageData(0, 0, 1, 1).data;
+
+  slide.style.cssText = `background-color: rgba(${r}, ${g}, ${b}, ${a/255});`;
+}
+
+imgs.forEach(img => {
+  setSlideBackground(img)
+  img.onload = () => setSlideBackground(img);
 });
 
